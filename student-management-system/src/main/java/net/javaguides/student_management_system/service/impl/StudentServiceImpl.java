76d8df2c5bc6ceec3c8a80c29c37070f9c9f1c04
Anjali <anjalivermaprem@gmail.com>
2024-11-06 19:40:33 +0530

    Student Management System

---
+++ student-management-system/src/main/java/net/javaguides/student_management_system/service/impl/StudentServiceImpl.java
@@ -0,0 +1,39 @@
+package net.javaguides.student_management_system.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import net.javaguides.student_management_system.entity.Student;
+import net.javaguides.student_management_system.repository.StudentRepository;
+import net.javaguides.student_management_system.service.StudentService;
+@Service
+public class StudentServiceImpl implements StudentService{
+private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+	@Override
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);
+		
+	}
+}
